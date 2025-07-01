b913aa55aa9fec8887f038486883fe8f46ffcf44
mirecl <grazhdankov@gmail.com>
2025-07-01 17:46:43 +0300

    Fix

--- tager.go
+++ tager.go
@@ -8,5 +8,6 @@ import (
 func TagMe() {
 	fmt.Println(debug.ReadBuildInfo())
 	buildifo, _ := debug.ReadBuildInfo()
-	fmt.Println(buildifo.Main)
+	fmt.Println(buildifo.Deps)
+	fmt.Println(buildifo.Deps[0].Version)
 }
