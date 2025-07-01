7c62850e36987399097660698246db8e641aa87f
mirecl <grazhdankov@gmail.com>
2025-07-01 18:08:23 +0300

    Fix

--- tager.go
+++ tager.go
@@ -2,11 +2,18 @@ package tager
 
 import (
 	"fmt"
+	"reflect"
+	"runtime"
 	"runtime/debug"
 )
 
 func TagMe() {
-	fmt.Println("Hello")
+	_, file, _, _ := runtime.Caller(0)
+	fmt.Println(file)
+	// Получаем имя текущего пакета через рефлексию
+	pkgPath := reflect.TypeOf(TagMe).PkgPath()
+	fmt.Println(pkgPath)
+
 	buildifo, _ := debug.ReadBuildInfo()
 	fmt.Println(buildifo.Deps[0].Version)
 }
